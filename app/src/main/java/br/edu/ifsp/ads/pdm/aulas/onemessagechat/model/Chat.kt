38c6f681d731e9bbe3c769771a62c62d64746d30
Eduardo0608 <eduardoaguliari0608@gmail.com>
2023-12-05 21:05:29 -0300

    feat: implement Chat class

---
+++ app/src/main/java/br/edu/ifsp/ads/pdm/aulas/onemessagechat/model/Chat.kt
@@ -0,0 +1,17 @@
+package br.edu.ifsp.ads.pdm.aulas.onemessagechat.model
+
+import android.os.Parcelable
+import androidx.annotation.NonNull
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+@Entity
+data class Chat(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = -1,
+
+    @NonNull
+    var message: String = ""
+) : Parcelable
